67b287db9cc33f20229f048dd5d00bf3be939c76
Gal Gofrit <gal.gofrit@gmail.com>
2024-09-17 18:17:04 +0900

    Comment out all ENABLE_ defines (to prepare for switch)

--- taiko_controller/taiko_controller.ino
+++ taiko_controller/taiko_controller.ino
@@ -5,33 +5,37 @@
 //#define DEBUG_TIME
 //#define DEBUG_DATA
 
-//#define ENABLE_KEYBOARD
-#define ENABLE_NS_JOYSTICK
+#define ENABLE_KEYBOARD
+//#define ENABLE_NS_JOYSTICK
 
 //#define HAS_BUTTONS
 
-#ifdef ENABLE_KEYBOARD
 #include <Keyboard.h>
-#endif
 
-#ifdef ENABLE_NS_JOYSTICK
+typedef enum mode_t {
+  MODE_KEYBOARD = 0,
+  MODE_JOYSTICK = 1
+} mode_t;
+const mode_t mode = 0; // 0: Keyboard, 1: Joystick
+
+/* #ifdef ENABLE_NS_JOYSTICK */
 #include "Joystick.h"
 const int led_pin[4] = {8, 9, 10, 11};
 const int sensor_button[4] = {SWITCH_BTN_ZL, SWITCH_BTN_LCLICK, SWITCH_BTN_RCLICK, SWITCH_BTN_ZR};
-#endif
+/* #endif */
 
 #ifdef HAS_BUTTONS
 int button_state[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int button_cd[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-#ifdef ENABLE_KEYBOARD
+/* #ifdef ENABLE_KEYBOARD */
 const int button_key[16] = {
   KEY_UP_ARROW, KEY_RIGHT_ARROW, KEY_DOWN_ARROW, KEY_LEFT_ARROW,
   'k', 'j', 'f', 'd',
   KEY_PAGE_DOWN, KEY_PAGE_UP, KEY_ESC, ' ',
   KEY_F1, 'q', 0 /*Fn1*/, 0 /*Fn2*/
 };
-#endif
-#ifdef ENABLE_NS_JOYSTICK
+/* #endif */
+/* #ifdef ENABLE_NS_JOYSTICK */
 const int button[16] = {
   0 /*SWITCH_HAT_U*/, 0 /*SWITCH_HAT_R*/, 0 /*SWITCH_HAT_D*/, 0 /*SWITCH_HAT_L*/,
   SWITCH_BTN_X, SWITCH_BTN_A, SWITCH_BTN_B, SWITCH_BTN_Y,
@@ -44,7 +48,7 @@ const int hat_mapping[16] = {
   SWITCH_HAT_L, SWITCH_HAT_UL, SWITCH_HAT_CENTER, SWITCH_HAT_U,
   SWITCH_HAT_DL, SWITCH_HAT_L, SWITCH_HAT_D, SWITCH_HAT_CENTER,
 };
-#endif
+/* #endif */
 #endif
 
 const int min_threshold = 15;
@@ -68,9 +72,9 @@ int raw[4] = {0, 0, 0, 0};
 float level[4] = {0, 0, 0, 0};
 long cd[4] = {0, 0, 0, 0};
 bool down[4] = {false, false, false, false};
-#ifdef ENABLE_NS_JOYSTICK
+/* #ifdef ENABLE_NS_JOYSTICK */
 uint8_t down_count[4] = {0, 0, 0, 0};
-#endif
+/* #endif */
 
 typedef unsigned long time_t;
 time_t t0 = 0;
@@ -98,13 +102,17 @@ void setup() {
   analogSwitchPin(pin[0]);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
-#ifdef ENABLE_NS_JOYSTICK
-  for (int i = 0; i < 8; ++i) pinMode(i, INPUT_PULLUP);
-  for (int i = 0; i < 4; ++i) {  digitalWrite(led_pin[i], HIGH); pinMode(led_pin[i], OUTPUT); }
-#endif
-#ifdef ENABLE_KEYBOARD
-  Keyboard.begin();
-#endif
+/* #ifdef ENABLE_NS_JOYSTICK */
+  if (mode == MODE_JOYSTICK) {
+    for (int i = 0; i < 8; ++i) pinMode(i, INPUT_PULLUP);
+    for (int i = 0; i < 4; ++i) {  digitalWrite(led_pin[i], HIGH); pinMode(led_pin[i], OUTPUT); }
+  }
+/* #endif */
+/* #ifdef ENABLE_KEYBOARD */
+  if (mode == MODE_KEYBOARD) {
+    Keyboard.begin();
+  }
+/* #endif */
   t0 = micros();
   Serial.begin(9600);
 }
@@ -172,23 +180,25 @@ void loop_test2() {
 
 void loop() {
   //loop_test2(); return;
-  
+
   static int si = 0;
 
-#ifdef ENABLE_KEYBOARD
-  parseSerial();
-#endif
-  
+/* #ifdef ENABLE_KEYBOARD */
+  if (mode == MODE_KEYBOARD) {
+    parseSerial();
+  }
+/* #endif */
+
   time_t t1 = micros();
   dt = t1 - t0;
   sdt += dt;
   t0 = t1;
-  
+
   float prev_level = level[si];
   sampleSingle(si);
   float new_level = level[si];
   level[si] = (level[si] + prev_level * 2) / 3;
-  
+
   threshold *= k_decay;
 
   for (int i = 0; i != 4; ++i) {
@@ -197,18 +207,20 @@ void loop() {
       if (cd[i] <= 0) {
         cd[i] = 0;
         if (down[i]) {
-#ifdef ENABLE_KEYBOARD
-          Keyboard.release(stageresult ? KEY_ESC : key[i]);
-#endif
+/* #ifdef ENABLE_KEYBOARD */
+          if (mode == MODE_KEYBOARD) {
+            Keyboard.release(stageresult ? KEY_ESC : key[i]);
+          }
+/* #endif */
           down[i] = false;
         }
       }
     }
   }
-  
+
   int i_max = 0;
   int level_max = 0;
-  
+
   for (int i = 0; i != 4; ++i) {
     if (level[i] > level_max && level[i] > threshold) {
       level_max = level[i];
@@ -229,24 +241,30 @@ void loop() {
         Serial.print(level[3], 1);
         Serial.print("\n");
 #endif
-#ifdef ENABLE_KEYBOARD
-        if (stageresult) {
-          Keyboard.press(KEY_ESC);
-        } else {
-          Keyboard.press(key[i_max]);
+/* #ifdef ENABLE_KEYBOARD */
+        if (mode == MODE_KEYBOARD) {
+          if (stageresult) {
+            Keyboard.press(KEY_ESC);
+          } else {
+            Keyboard.press(key[i_max]);
+          }
         }
-#endif
+/* #endif */
         down[i_max] = true;
-#ifdef ENABLE_NS_JOYSTICK
-        if (down_count[i_max] <= 2) down_count[i_max] += 2;
-#endif
+/* #ifdef ENABLE_NS_JOYSTICK */
+        if (mode == MODE_JOYSTICK) {
+          if (down_count[i_max] <= 2) down_count[i_max] += 2;
+        }
+/* #endif */
       }
       for (int i = 0; i != 4; ++i)
         cd[i] = cd_length;
-#ifdef ENABLE_KEYBOARD
-      if (stageselect)
-        cd[i_max] = cd_stageselect;
-#endif
+/* #ifdef ENABLE_KEYBOARD */
+      if (mode == MODE_KEYBOARD) {
+        if (stageselect)
+          cd[i_max] = cd_stageselect;
+      }
+/* #endif */
     }
     sdt = 0;
   }
@@ -254,7 +272,7 @@ void loop() {
   if (cd[i_max] > 0) {
     threshold = max(threshold, level_max * k_threshold);
   }
-  
+
   static time_t ct = 0;
   static int cc = 0;
   ct += dt;
@@ -267,7 +285,7 @@ void loop() {
   bi = ((bi+1)&3);
   pinMode(bi+4, OUTPUT);
   digitalWrite(bi+4, LOW);
-    
+
   int state;
   int* bs = button_state + (bi << 2);
   int* bc = button_cd + (bi << 2);
@@ -281,45 +299,50 @@ void loop() {
     if (state != bs[i] && bc[i] == 0) {
       bs[i] = state;
       bc[i] = 15000;
-#ifdef ENABLE_KEYBOARD
-      if (state) {
-        Keyboard.press(button_key[(bi << 2) + i]);
-      } else {
-        Keyboard.release(button_key[(bi << 2) + i]);
+/* #ifdef ENABLE_KEYBOARD */
+      if (mode == MODE_KEYBOARD) {
+        if (state) {
+          Keyboard.press(button_key[(bi << 2) + i]);
+        } else {
+          Keyboard.release(button_key[(bi << 2) + i]);
+        }
       }
-#endif
+/* #endif */
     }
-#ifdef ENABLE_NS_JOYSTICK
-    Joystick.Button |= (bs[i] ? button[(bi << 2) + i] : SWITCH_BTN_NONE);
-#endif
+/* #ifdef ENABLE_NS_JOYSTICK */
+    if (mode == MODE_JOYSTICK) {
+      Joystick.Button |= (bs[i] ? button[(bi << 2) + i] : SWITCH_BTN_NONE);
+    }
+/* #endif */
   }
 #endif
-  
-#ifdef ENABLE_NS_JOYSTICK
-  if (ct > 32000 || (ct > 8000 && (down_count[0] || down_count[1] || down_count[2] || down_count[3]))) {
-    for (int i = 0; i < 4; ++i) { // Sensors
-      bool state = (down_count[i] & 1);
-      Joystick.Button |= (state ? sensor_button[i] : SWITCH_BTN_NONE);
-      down_count[i] -= !!down_count[i];
-      digitalWrite(led_pin[i], state ? LOW : HIGH);
-    }
+
+  if (mode == MODE_JOYSTICK)
+  {
+    if (ct > 32000 || (ct > 8000 && (down_count[0] || down_count[1] || down_count[2] || down_count[3]))) {
+      for (int i = 0; i < 4; ++i) { // Sensors
+        bool state = (down_count[i] & 1);
+        Joystick.Button |= (state ? sensor_button[i] : SWITCH_BTN_NONE);
+        down_count[i] -= !!down_count[i];
+        digitalWrite(led_pin[i], state ? LOW : HIGH);
+      }
 #ifdef HAS_BUTTONS
-    state = 0;
-    for (int i = 0; i < 4; ++i) { // Buttons for hats
-      state |= (button_state[i] ? 1 << i : 0);
-    }
-    Joystick.HAT = hat_mapping[state]; 
+      state = 0;
+      for (int i = 0; i < 4; ++i) { // Buttons for hats
+        state |= (button_state[i] ? 1 << i : 0);
+      }
+      Joystick.HAT = hat_mapping[state];
 #endif
-    Joystick.sendState();
-    Joystick.Button = SWITCH_BTN_NONE;
+      Joystick.sendState();
+      Joystick.Button = SWITCH_BTN_NONE;
 #ifdef DEBUG_TIME
-    if (cc > 0)
-      Serial.println((float)ct/cc);
+      if (cc > 0)
+        Serial.println((float)ct/cc);
 #endif
-    ct = 0;
-    cc = 0;
+      ct = 0;
+      cc = 0;
+    }
   }
-#endif
 
 #ifdef DEBUG_OUTPUT
   static bool printing = false;
@@ -349,7 +372,7 @@ void loop() {
       Serial.println();
       printing = false;
     }
-  } 
+  }
 #endif
 
   level[si] = new_level;
@@ -357,5 +380,5 @@ void loop() {
 
   long ddt = 300 - (micros() - t0);
   if(ddt > 3) delayMicroseconds(ddt);
-  
+
 }
